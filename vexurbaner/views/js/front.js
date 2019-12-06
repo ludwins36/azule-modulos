16623a7f9d2255ed31eba996ed2720f658610555
OscarG <ogalloval@gmail.com>
2019-12-05 23:50:30 -0500

    a

--- vexurbaner/views/js/front.js
+++ vexurbaner/views/js/front.js
@@ -32,8 +32,7 @@ function initMapUrbaner() {
     destination.addListener('dragend', (event) => {
         console.log(directionsDisplay);
         
-        let address = directionsDisplay.getDirections();
-        console.log(address);
+        console.log(directionsDisplay.getDirections());
 
         // input.value = directionsDisplay.getDirections().routes[0].legs[0].end_address;
         // input2.value = directionsDisplay.getDirections().routes[0].legs[0].end_address;
