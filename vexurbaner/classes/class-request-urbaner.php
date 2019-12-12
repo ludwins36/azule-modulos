7bc8037a71b6d2f0da7e9ce9113b789383d63b73
unknown <ludwins36@gmail.com>
2019-12-11 19:58:59 -0500

    a

--- vexurbaner/classes/class-request-urbaner.php
+++ vexurbaner/classes/class-request-urbaner.php
@@ -269,10 +269,10 @@ class VexUrbanerRequest
                 $message .= "\n";
                 $message .= "\n";
             }
+            $message .= 'a nombre de ' . $data['contact'] . ', Documento ' . $data['dni'];
 
         }
 
-        $message .= 'a nombre de ' . $data['contact'] . ', Documento ' . $data['dni'];
             $message .= "\n";
             $message .= 'Comentarios: ' . $this->context->cookie->__get('message');
 
@@ -338,7 +338,7 @@ class VexUrbanerRequest
                 $message .= "\n";
             }
 
-            $message .= 'a nombre de ' . $data['contact'] . ', Documento ' . $data['dni'];
+            // $message .= 'a nombre de ' . $data['contact'] . ', Documento ' . $data['dni'];
             
         }
         $message .= "\n";
